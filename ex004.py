f80367a3d1bf2612872ade96acb40b0ab547e089
Valmir Santana <valmirsantanape@gmail.com>
2020-08-11 20:37:38 -0300

    ex004.py
    Exercicio Resolvido

--- ex004.py
+++ ex004.py
@@ -1,5 +1,7 @@
+import math
+
 n1 = int(input('Digite um numero: '))
 print('Você digitou o numeo {} \n'
       ' O dobro equivale a {}\n'
       'O triplo equivale a {}\n'
-      'O quadruplo equivale a {}'.format(n1, n1*2, n1*3, n1*4))
+      'E a raiz quadrada equivale a {}'.format(n1, n1*2, n1*3, math.sqrt(n1)))
