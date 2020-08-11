57abe662a29a0cb7a2cff6bb1445101220e9e49b
Valmir Santana <valmirsantanape@gmail.com>
2020-08-11 20:22:08 -0300

    Exercicio3Resolvido

--- ex002.py
+++
@@ -1,5 +0,0 @@
-algo = input('Digite algo: ')
-print('É alphanumerio: ', algo.isalnum())
-print('É apenas letra ', algo.isalpha())
-
-print('É apenas numero? ',algo.isnumeric())
---
+++ ex003.py
@@ -0,0 +1,2 @@
+n1 = int(input('Digite um numero:' ))
+print('Voce digitiou o umero {}, que tem como antecssor {} e como sucessor {}'.format(n1, n1-1, n1+1) )
