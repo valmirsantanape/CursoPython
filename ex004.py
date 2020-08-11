bcab27e9256879a8ef41e4bec5a502731571bc7b
Valmir Santana <valmirsantanape@gmail.com>
2020-08-11 20:27:33 -0300

    Create ex004.py
    Exercicio Resolvdo

---
+++ ex004.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um numero: '))
+print('Você digitou o numeo {} \n'
+      ' O dobro equivale a {}\n'
+      'O triplo equivale a {}\n'
+      'O quadruplo equivale a {}'.format(n1, n1*2, n1*3, n1*4))
