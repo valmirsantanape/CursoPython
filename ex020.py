6c96bafb2402b2ef9644736ac5c872e671aa308d
Valmir Santana <valmirsantanape@gmail.com>
2020-08-17 22:36:08 -0300

    ex020.py
    sorteando nomes

--- ex020.py
+++ ex020.py
@@ -1,6 +1,9 @@
+from random import shuffle
 al1 = input('Primeiro Aluno: ')
 al2 = input('Segundo Aluno: ')
 al3 = input('Terceiro Aluno: ')
 al4 = input('Quarto Aluno: ')
 lista = [al1, al2, al3, al4]
+shuffle(lista)
+print('A ordem de apresentacao será: ')
 print(lista)
