d2fba10523ef363aa8c68a179567165c40572c52
Valmir Santana <valmirsantanape@gmail.com>
2020-08-22 01:37:32 -0300

    ex023.py
    informacoes de um numero digitado

--- ex0023.py
+++ ex0023.py
@@ -1,5 +1,34 @@
-numero = input('Digie um numero entre 0 e 9999: ')
-print('Casa de unidade: ',numero[3])
-print('Casa de dezena: ',numero[2])
-print('Casa de centena:' , numero[1])
-print('Casa de milhar: ', numero[0])
+resp = 's'
+while(resp == 's'):
+    numero = int(input('Digie um numero entre 0 e 9999: '))
+    if numero < 1:
+        print('Digite um numer inteiro!')
+    u = numero // 1 % 10
+    d = numero // 10 % 10
+    c = numero // 100 % 10
+    m = numero // 1000 % 10
+
+
+    if m < 1 and c < 1 and d < 1:
+        print('Unidade: ', u)
+#       print('Não dezena ')
+#       print('Não tem centena')
+#       print('Não tem milhar')
+    elif m < 1 and c < 1:
+        print('Unidade: ', u)
+        print('dezena ', d)
+#       print('Não tem centena')
+#       print('Não tem milhar')
+    elif m < 1:
+        print('Unidade: ', u)
+        print('dezena ', d)
+        print('centena',c)
+#       print('Não tem milhar')
+    else:
+        print('Unidade: ', u)
+        print('dezena ', d)
+        print('Centena', c)
+        print('Milhar', m)
+    resp = input('Deseja continuar? s/n \n')
+print('Obrigado por usar nosso sistema. \n'
+      'Até mais!')
--- ex022.py
+++ ex022.py
@@ -1,27 +1,7 @@
-frase = 'Curso em Video Python'
-print('Manipulando String')
-
-print(frase[9])
-print(frase[9:14])
-print(frase[9:20:2])
-print(frase[15:])
-print(frase[:5])
-
-print(len(frase))
-print('frase.count(','o',') pega a letra indicada no paramentro'
-       ' e retorna quantas foram encontradas'
-                         '. No exemplo foram encontradas  ',  frase.count('o'),'letras do tipo')
-print(frase.find('curso'))
-print(frase.replace('Python', "Android"))
-print(frase.upper())
-print(frase.lower())
-
-print(frase.capitalize())
-print(frase.title())
-print(frase.strip())
-nome = input('Digite seu nome completo')
-print(nome)
-print('Nome com todas asletras maiusculas', nome.upper())
-print('Nome com todas asletras minusculas', nome.lower())
-print('Total de caractere considerando os espaços: ', len(nome.strip()))
-print('Total de carctere sem considerar os espaços: ', len(nome))
+nome = input('Digite seu nome completo: \n').strip()
+print('Analizando seu nome.. ')
+print('Nome com todas as letras maiusculas: ', nome.upper())
+print('Nome com todas as letras minusculas: ', nome.lower())
+print('Total de caracteres considerando os espaços: ', len(nome))
+print('Total de carcteres sem considerar os espaços: ', len(nome)-nome.count(' '))
+print('Seu primeir nome tem {} letras' .format(nome.find(' ')))
