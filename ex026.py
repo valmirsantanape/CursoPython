41e1bcc3f22da230d4c406b1e8c4a88d7696325e
Valmir Santana <valmirsantanape@gmail.com>
2020-08-25 20:48:53 -0300

    ex026.py

--- ex026.py
+++ ex026.py
@@ -1,2 +1,4 @@
-frase = str(input('Digite uma frase: \n'))
+frase = str(input('Digite uma frase: \n')).strip()
 print('A  letra ''''a ''''aparece {} vezes na frase ' .format(frase.upper().count('A')))
+print('A primeira letra A apareceu na posição {} ' .format(frase.upper().find('A')+1))
+print('A ultima letra A apareceu na posição {}'.format(frase.upper().rfind('A')+1))
