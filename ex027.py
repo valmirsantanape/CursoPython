47b79bcecc7e177a5fbca96dc532c232187a3031
Valmir Santana <valmirsantanape@gmail.com>
2020-08-27 23:58:49 -0300

    update

---
+++ ex027.py
@@ -0,0 +1,5 @@
+nome = str(input('Digite seu nome: \n')).strip()
+print('Muito prazer em conhecer você, {}.' .format(nome))
+nome = nome.split()
+print('Seu primeiro nome é {}'.format(nome[0]))
+print( 'Seu ultimo nome é {}'.format(nome[-1]))
