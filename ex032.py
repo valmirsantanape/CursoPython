3c164988fc16955bfd872b12c77c12d9e8470d05
Valmir Santana <valmirsantanape@gmail.com>
2020-08-29 15:14:25 -0300

    Update ex032.py

--- ex032.py
+++ ex032.py
@@ -1,7 +1,8 @@
-ano = int(input('Digite o ano que deseja analisar: '))
+from datetime import date
+ano = int(input('Que ano que deseja analisar? Digite 0 para analisar o no atual'))
+if ano == 0:
+    ano = date.today().year
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
-
     print('O ano {} é bissexto'.format(ano))
-
 else:
     print('O ano {} não é bissexto'.format(ano))
