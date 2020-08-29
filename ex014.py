49d35af7a78e0a4382166dddd51f1c044537e238
Valmir Santana <valmirsantanape@gmail.com>
2020-08-29 14:24:07 -0300

    ex019.py

--- ex014.py
+++ ex014.py
@@ -1,4 +1,11 @@
-import random;
-nome = ['Tiago','Fabio','Marcelo','Antonio']
-num = int(random.randint(0,3))
+from random import randint
+from time import sleep
+n1 = str(input('Digite o nome do primeiro aluno: '))
+n2 = str(input('Digite o nome do segundo aluno: '))
+n3 = str(input('Digite o nome do terceiro aluno: '))
+n4 = str(input('Digite o nome do quarto aluno: '))
+nome = [n1,n2,n3,n4]
+print('sorteando...')
+sleep(3)
+num = int(randint(0,3))
 print(nome[num])
