4ea33811433ebab633f4b4032a40d9e164a955f6
Valmir Santana <valmirsantanape@gmail.com>
2020-08-29 14:55:54 -0300

    ex032.py

--- ex032.py
+++ ex032.py
@@ -1,4 +1,5 @@
-ano = int(input('Digite o ano: '))
-if ano % 5 == 0:
+ano = int(input('Digite o ano que deseja analisar: '))
+if ano % 4 == 0:
     print('O ano de {} é um ano bissexto '.format(ano))
-else: print('O ano de {} não é bissexto'.format(ano))
+else:
+    print('O ano de {} não é bissexto'.format(ano))
