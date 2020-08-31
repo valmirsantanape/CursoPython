0a9da0893dad7861833612302783b122369706f9
Valmir Santana <valmirsantanape@gmail.com>
2020-08-31 17:46:25 -0300

    ex035.py

--- ex035.py
+++ ex035.py
@@ -1,3 +1,8 @@
 reta1 = float(input('Digite a primeira reta'))
 reta2 = float(input('Digite a primeira reta'))
-reta2 = float(input('Digite a primeira reta'))
+reta3 = float(input('Digite a primeira reta'))
+
+if reta1 < reta2 + reta3 and reta2 < reta1 + reta3 and reta3 < reta1 + reta2:
+    print('As retas indicadas podem formar um triângulo. ')
+else:
+    print('A retas indicadas não podem formar um trângulo. ')
