518b72811546638bbbf4f564d42478f8c0bd9ac5
Valmir Santana <valmirsantanape@gmail.com>
2020-09-02 19:19:10 -0300

    Update ex037-conversor.py

--- ex037-conversor.py
+++ ex037-conversor.py
@@ -5,10 +5,10 @@ escolha = int(input('Digite 1 para binário\n'
                     'Digite 3 para hexadecimal\n'
                     '>>>>'))
 if escolha == 1:
-    print('Binário')
+    print('O numero {} convertido para binário é {}.'.format(numero, bin(numero)))
 elif escolha == 2:
-    print('Octal')
+    print('O numero {} convertido para octal é {}.'.format(numero, oct(numero)))
 elif escolha == 3:
-    print('Hexadecimal')
+    print('O numero {} convertido para hexdecimal é {}.'.format(numero, hex(numero)))
 else:
     print('Escolha inválida ')
