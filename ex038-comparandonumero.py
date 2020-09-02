9dcee887daf9e84c4e31f81623eed7bc306a003c
Valmir Santana <valmirsantanape@gmail.com>
2020-09-02 19:26:45 -0300

    Update ex038-comparandonumero.py

---
+++ ex038-comparandonumero.py
@@ -0,0 +1,8 @@
+num1 = float(input('Digite o primeiro numero: \n'))
+num2 = float(input('Digite o segundo numero: \n'))
+if num1 > num2:
+    print('O primeiro valor e maior')
+elif num2 > num1:
+    print('O segundo valor é maior')
+else:
+    print('Os valores são iguais')
