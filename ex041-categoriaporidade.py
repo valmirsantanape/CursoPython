7a4918cdf5db7fa6eda38a5a77f6bd3f2a51cbea
Valmir Santana <valmirsantanape@gmail.com>
2020-09-04 01:43:57 -0300

    ex041-categoriaporidade.py

--- ex041-categoriaporidade.py
+++ ex041-categoriaporidade.py
@@ -1,4 +1,19 @@
 from datetime import date
-nasc = int(input('Digite seu ano de nascimento:\n'))
+nasc = int(input('Digite seu ano de nascimento:  \n'))
 anoatual = date.today().year
 idade = anoatual - nasc
+if idade <= 9:
+    print('Idade: {}'.format(idade))
+    print('Mirim')
+elif idade <= 14:
+    print('Idade: {}'.format(idade))
+    print('Infantil')
+elif idade <= 19:
+    print('Idade: {}'.format(idade))
+    print('Junior')
+elif idade <= 25:
+    print('Idade: {}'.format(idade))
+    print('Senior')
+else:
+    print('Idade: {}'.format(idade))
+    print('Master')
