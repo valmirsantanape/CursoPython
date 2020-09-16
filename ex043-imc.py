2c51a5856e8e00b1c5370459761a7dd929f8d68b
Valmir Santana <valmirsantanape@gmail.com>
2020-09-15 22:43:45 -0300

    Update ex043-imc.py

--- ex043-imc.py
+++ ex043-imc.py
@@ -12,8 +12,7 @@ elif imc >= 30 and imc < 40:
     print('Obesidade')
 else:
     print('Obesidade mórbida')
-#Exercício Python 43: Desenvolva uma lógica que leia o peso e a altura de uma pessoa,
-#calcule seu Índice de Massa Corporal (IMC) e mostre seu status, de acordo com a tabela abaixo:
+
 
 #– IMC abaixo de 18,5: Abaixo do Peso
 
