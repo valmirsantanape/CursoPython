337656f0fc76f78caa28da756f10358a1b705089
Valmir Santana <valmirsantanape@gmail.com>
2020-09-16 22:38:59 -0300

    Update ex048-somanumeroimpares.py

--- ex048-somanumeroimpares.py
+++ ex048-somanumeroimpares.py
@@ -1,9 +1,8 @@
-print('Somas dos numeros impares entre 0 e 500')
+print('Somas dos numeros impares multiplos de 3 entre 0 e 500')
 s = 0
 cont = 0
 for c in range(1, 500, 2):
     if c % 3 == 0:
-#        cont = cont + 1
-        s = s + c
-print(s)
-print(cont)
+        cont += 1
+        s += c
+print('A soma de todos os {} valores solicitados é {}'.format(cont, s))
