08ac7d3a540073fb0f6ffee21916e505d7cbd042
Valmir Santana <valmirsantanape@gmail.com>
2020-09-18 20:46:49 -0300

    Create ex050-somadospares.py

---
+++ ex050-somadospares.py
@@ -0,0 +1,7 @@
+num1 = int(input('Digite o primeiro numero: \n'))
+num2 = int(input('Digite o segundo numero: \n'))
+num3 = int(input('Digite o terceiro numero: \n'))
+num4 = int(input('Digite o quanrto numero: \n'))
+num5 = int(input('Digite o quinto numero: \n'))
+ for c in range()
+     
