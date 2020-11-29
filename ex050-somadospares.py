42bb16d344feb7f7c38695428166cc96ed5ae77e
Valmir Santana <valmirsantanape@gmail.com>
2020-11-29 19:29:38 -0300

    update

--- ex050-somadospares.py
+++ ex050-somadospares.py
@@ -1,8 +1,9 @@
 soma = 0
 cont = 0
 for c in range(1, 7):
-    num = int(input('Digite um {}º valor: '.format(c)))
-    if num% 2 == 0:
-        soma = soma + num
-        cont = cont + 1
-print('Vcê informou {} numeros pares e a soma desses numeros é {}.'.format(cont, soma))
+    num = int(input('Digite o {}º valor '.format(c)))
+    if num % 2 == 0:
+        soma += num
+        cont += 1
+print('Você informou {} numeros pares. \n'
+      'A soma desses numeros é {}.'.format(cont, soma))
