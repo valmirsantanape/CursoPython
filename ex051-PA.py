aa915cd6ccf57f5b46c127cd3eeeb317dad2c1ba
Valmir Santana <valmirsantanape@gmail.com>
2020-11-29 19:55:07 -0300

    update

--- ex051-PA.py
+++ ex051-PA.py
@@ -1,8 +1,10 @@
 #Exercício Python 51: Desenvolva um programa que leia o primeiro termo e a razão de uma PA.
 # No final, mostre os 10 primeiros termos dessa progressão.
-cont = 0
 a1 = int(input('Digite o primeiro termo da sua prograssão aritmética: \n'))
 razao = int(input('Informe qual a razão da PA: '))
-for c in range(1, 11):
-    print(a1)
+n = int(input('Digite o numero de termos da sua PA'))
+
+for c in range(1, n + 1):
+    print('{}'.format(a1), end=' ')
     a1 = a1 + razao
+print('\nACABOU!')
