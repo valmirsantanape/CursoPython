2bd9bc1b6d4174cb556b613cdcd9ee0289d4e124
Valmir Santana <valmirsantanape@gmail.com>
2020-11-29 23:44:35 -0300

    Create ex053-palindromo.py
    Palíndromo

---
+++ ex053-palindromo.py
@@ -0,0 +1,14 @@
+frase = str(input('Digite ma frase: \n')).strip().upper()
+print(frase)
+palavras = frase.split() #metodo split transforma a strng em uma lista tendo espaço como separador dos itens da lista
+print(palavras)
+junto = ''.join(palavras) #metodo 'join' separa as strings com o simbolo indicado entre as aspas
+print(junto)
+inverso = ''
+for letra in range(len(junto)-1, -1, -1): #metodo len define o total de caracteres da string
+    inverso = inverso + junto[letra]
+print(junto, inverso)
+if inverso == junto:
+    print('Temos uma palídromo! ')
+else:
+    print('A frase digitada não é um palindromo.')
