9a99fa9f0206e6729f48d1666988906d33788d63
Valmir Santana <valmirsantanape@gmail.com>
2020-11-29 23:45:48 -0300

    Update ex053-palindromo.py
    verificar palíndromo sem estrutura de repeticao

--- ex053-palindromo.py
+++ ex053-palindromo.py
@@ -4,9 +4,9 @@ palavras = frase.split() #metodo split transforma a strng em uma lista tendo esp
 print(palavras)
 junto = ''.join(palavras) #metodo 'join' separa as strings com o simbolo indicado entre as aspas
 print(junto)
-inverso = ''
-for letra in range(len(junto)-1, -1, -1): #metodo len define o total de caracteres da string
-    inverso = inverso + junto[letra]
+inverso = junto[::-1]
+'''for letra in range(len(junto)-1, -1, -1): #metodo len define o total de caracteres da string
+    inverso = inverso + junto[letra]'''
 print(junto, inverso)
 if inverso == junto:
     print('Temos uma palídromo! ')
