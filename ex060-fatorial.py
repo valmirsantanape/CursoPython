00b3c6fbb657f7ca0f00cd4053ca18f27c77f1ef
Valmir Santana <valmirsantanape@gmail.com>
2020-12-07 07:51:04 -0300

    update

--- ex060-fatorial.py
+++ ex060-fatorial.py
@@ -1,8 +1,10 @@
-num1 = int(input('Digte um numro e descubra seu fatorial '))
-num2 = num1
-#5! = 5*4*3*2*1
-fat = 1
-while num2 > 0:
-    fat = fat * num2
-    num2 += -1
-print('O fatorial de {} é igual a {}'.format(num1, fat))
+from math import factorial
+num = int(input('Digite um numero e descubra seu fatorial: \n'))
+fat = factorial(num)
+c = num
+print('{}! ='.format(num), end=' ')
+while c > 0:
+    print('{} '.format(c), end='')
+    print('*' if c > 1 else '=', end=' ' )
+    c -= 1
+print(fat)
