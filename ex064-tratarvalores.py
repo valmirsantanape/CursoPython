08d94868e751549411e66c7d8024dc6540319ec2
Valmir Santana <valmirsantanape@gmail.com>
2020-12-07 18:42:15 -0300

    Create ex064-tratarvalores.py

---
+++ ex064-tratarvalores.py
@@ -0,0 +1,9 @@
+num = 0
+soma = 0
+tot = 0
+while num != 999:
+    num = int(input('Digite um nmero [999 para parar]'))
+    soma = soma + num
+    tot = tot + 1
+print('O total de numeros digitados foram {}\'soma - 999)
+print('FIM')
