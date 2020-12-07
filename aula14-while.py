5308ac29ae92a2686ebdb63eba3869e100c78954
Valmir Santana <valmirsantanape@gmail.com>
2020-12-06 23:06:24 -0300

    estruturaenquanto-while

---
+++ aula14-while.py
@@ -0,0 +1,13 @@
+'''
+Contando até 10 com comando For:
+for c in range(1, 11):
+    print(c)
+print('fim')
+
+Iciando com estrutura While
+'''
+c = 1
+while c <= 10:
+    print(c)
+    c += 1
+print('fim')
