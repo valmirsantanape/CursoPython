0c6edbaf9450a1cd5964b905a48e31a425d44d89
Valmir Santana <valmirsantanape@gmail.com>
2020-12-07 19:49:03 -0300

    update

--- ex064-tratarvalores.py
+++ ex064-tratarvalores.py
@@ -5,5 +5,7 @@ while num != 999:
     num = int(input('Digite um nmero [999 para parar]'))
     soma = soma + num
     tot = tot + 1
-print('O total de numeros digitados foram {}\'soma - 999)
+totsoma = soma - 999
+print('O total de numeros digitados foram {}\n'
+      'A soma desses numeros é igual a {}'.format(tot - 1, totsoma))
 print('FIM')
