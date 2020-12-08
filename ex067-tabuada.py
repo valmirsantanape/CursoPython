7c098a84150d81b2d5dbb3aca0b81ff3c3a07c93
Valmir Santana <valmirsantanape@gmail.com>
2020-12-08 20:04:15 -0300

    ex067-tabuada.py

--- ex067-tabuada.py
+++ ex067-tabuada.py
@@ -1,28 +1,30 @@
-num = int(input('Digite um numero: '))
-cont = soma  = 0
-'''print('------ SOMA -------')
-while cont <= 10:
-    soma = soma + num
-    print(f'{num} + {cont} = {soma}')
-    cont = cont + 1
-print('\n------ SUBTRAIR -------')
-cont = 10
-sub = 0
-while cont >= 0:
-    sub = cont - num
-    print(f'{cont} - {num} = {sub}')
-    cont = cont - 1
-print('\n------ Multiplicar -------')
-cont = mult = 0
-while cont <= 10:
-    mult = num * cont
-    print(f'{num} x {cont} = {mult}')
-    cont = cont + 1
-'''
-print('\n------ Dividir -------')
-cont = div = 1
-while cont <= 10:
-    if num % 2 == 0:
-        div = num / cont
-        print(f'{num} : {cont} = {div}')
-    cont += 1
+cont = soma = 0
+while True:
+    num = int(input('Digite um numero: '))
+    if num < 0:
+        break
+    print('------ SOMA -------')
+    while cont <= 10:
+        soma = soma + num
+        print(f'{num} + {cont} = {soma}')
+        cont = cont + 1
+    print('\n------ SUBTRAIR -------')
+    cont = 10
+    sub = 0
+    while cont >= 0:
+        sub = num - cont
+        print(f'{num} - {cont} = {sub}')
+        cont = cont - 1
+    print('\n------ Multiplicar -------')
+    cont = mult = 0
+    while cont <= 10:
+        mult = num * cont
+        print(f'{num} x {cont} = {mult}')
+        cont = cont + 1
+    print('\n------ Dividir -------')
+    cont = div = 1
+    while cont <= 10:
+        if num % cont == 0:
+            div = num / cont
+            print(f'{num} : {cont} = {div}')
+        cont += 1
