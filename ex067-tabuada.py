fb1c5c47b9a53a8914726d30b78d16cd20ab666e
Valmir Santana <valmirsantanape@gmail.com>
2020-12-08 19:36:44 -0300

    ex067-tabuada

---
+++ ex067-tabuada.py
@@ -0,0 +1,28 @@
+num = int(input('Digite um numero: '))
+cont = soma  = 0
+'''print('------ SOMA -------')
+while cont <= 10:
+    soma = soma + num
+    print(f'{num} + {cont} = {soma}')
+    cont = cont + 1
+print('\n------ SUBTRAIR -------')
+cont = 10
+sub = 0
+while cont >= 0:
+    sub = cont - num
+    print(f'{cont} - {num} = {sub}')
+    cont = cont - 1
+print('\n------ Multiplicar -------')
+cont = mult = 0
+while cont <= 10:
+    mult = num * cont
+    print(f'{num} x {cont} = {mult}')
+    cont = cont + 1
+'''
+print('\n------ Dividir -------')
+cont = div = 1
+while cont <= 10:
+    if num % 2 == 0:
+        div = num / cont
+        print(f'{num} : {cont} = {div}')
+    cont += 1
