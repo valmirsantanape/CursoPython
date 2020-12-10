3508e0295c197f3b7b0b72d76bcfd7678c9cccd1
Valmir Santana <valmirsantanape@gmail.com>
2020-12-10 20:35:08 -0300

    update

--- aula16-tuplas.py
+++ aula16-tuplas.py
@@ -1,1 +1,18 @@
-lanche = ('hamburguer')
+lanche = 'hamburguer', 'Suco', 'Pizza', 'Pudim', 'batata frita'
+for cont in range(0, len(lanche)):
+    print(f'Eu vou comer {lanche[cont]}')
+
+for comida in lanche:
+    print(f'Eu vou comer {comida}')
+
+for pos, comida in enumerate(lanche):
+    print(f'Eu vou comer {comida} na posição {pos}')
+
+print(f'Comi pra caramba!')
+
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = a + b
+d = b + a 
+print(c)
+print(d)
