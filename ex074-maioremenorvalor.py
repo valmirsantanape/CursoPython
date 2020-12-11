7d79cd48b07389a41d9407c70b0b2a51b9626eba
Valmir Santana <valmirsantanape@gmail.com>
2020-12-10 23:21:00 -0300

    update

--- ex074-maioremenorvalor.py
+++ ex074-maioremenorvalor.py
@@ -1,2 +1,5 @@
 from random import randint
-num1 = randint()
+num = (randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9), randint(0, 9))
+print(num)
+print(f'O maior numero sorteado foi {max(num)}')
+print(f'E o menor numero sorteado foi {min(num)}')
