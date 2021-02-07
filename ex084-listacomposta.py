fb9a15085f119fd1fd6b378a10ec0304b929ee90
valmirsantanape <valmirsantanape@gmail.com>
2021-02-07 15:26:42 -0300

    ex084-listacomposta.py

--- ex084-listacomposta.py
+++ ex084-listacomposta.py
@@ -6,10 +6,10 @@ while True:
     temp.append(str(input('Nome: ')))
     temp.append(float(input('Peso: ')))
     if len(pessoas) == 0:
-        mairpeso = menorpeso = temp[1]
+        maiorpeso = menorpeso = temp[1]
     else:
-        if temp[1] > mairpeso:
-            mairpeso= temp[1]
+        if temp[1] > maiorpeso:
+            maiorpeso= temp[1]
         if temp[1] < menorpeso:
             menorpeso = temp[1]
     pessoas.append(temp[:])
@@ -21,12 +21,12 @@ print('=-' * 20)
 print(f'{len(pessoas)} pessoas foram cadastradas')
 print(f'Dados cadastrados: \n{pessoas}')
 print()
-print(f'O maior peso foi de {mairpeso}kg. Peso de ', end='')
+print(f'O maior peso foi de {maiorpeso}kg. Peso de ', end='')
 for p in pessoas:
-    if p[1] == mairpeso:
+    if p[1] == maiorpeso:
         print(f'[{p[0]}] ', end='')
 print()
 print(f'O menor peso foi de {menorpeso}kg. Peso de ', end='')
 for p in pessoas:
     if p[1] == menorpeso:
-        print(f'[{p[0]}] ',end='')
+        print(f'[{p[0]}] ', end='')
