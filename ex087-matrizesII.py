1de0eed8dc70d95470bf23965cd91e3f5d445b4a
valmirsantanape <valmirsantanape@gmail.com>
2021-02-07 19:53:22 -0300

    ex087-matrizesII.py

--- ex087-matrizesII.py
+++ ex087-matrizesII.py
@@ -6,6 +6,12 @@ for l in range(0, 3):
         if matriz[l][c] % 2 == 0:
             somaPar = somaPar + matriz[l][c]
         somaCol3 = matriz[0][2] + matriz[1][2] + matriz[2][2]
+        if matriz[1][0] >= matriz[1][1] and matriz[1][0] >= matriz[1][2]:
+            maiorCol2 = matriz[1][0]
+        if matriz[1][1] >= matriz[1][2]:
+            maiorCol2 = matriz[1][1]
+        else:
+            maiorCol2 = matriz[1][2]
 for l in range(0, 3):
     for c in range(0, 3):
         print(f'[{matriz[l][c]:^5}]', end='')
@@ -13,3 +19,4 @@ for l in range(0, 3):
 print('=-' * 20)
 print(f'Soma dos numeros pares: {somaPar}')
 print(f'Som dos numeros da terceira coluna: {somaCol3}')
+print(f'Maior numero da coluna 2 é: {maiorCol2}')
